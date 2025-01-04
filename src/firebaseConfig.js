3b5f9b60d6dafd97c75ee0b4d15facb873da31ad
Tarun Joshi <114671630+tarunjoshi730@users.noreply.github.com>
2025-01-04 15:55:14 +0000

    Responsive_dashboard_with_with_news_and_Payout_fratures

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,18 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCC0IJa4CO1Jkj1cVHYik4NjGEKzlCVi5o",
- authDomain: "project-dashboard-97b90.firebaseapp.com",
- projectId: "project-dashboard-97b90",
- storageBucket: "project-dashboard-97b90.firebasestorage.app",
- messagingSenderId: "41910588399",
- appId: "1:41910588399:web:19ed839be66d764f8e9c1a",
- measurementId: "G-7ZMC8RL0SY"
+  apiKey: "AIzaSyAXTVK6qfbXezM9zQAiAe4kj8l9v6qRqwo",
+  authDomain: "gen-lang-client-0703583468.firebaseapp.com",
+  projectId: "gen-lang-client-0703583468",
+  storageBucket: "gen-lang-client-0703583468.firebasestorage.app",
+  messagingSenderId: "109637390605",
+  appId: "1:109637390605:web:dc50833d0a66ce7e6be044",
+  measurementId: "G-VQXVDRSRG3"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
